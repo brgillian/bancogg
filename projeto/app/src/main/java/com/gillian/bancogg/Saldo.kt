04aae082dc5fbb946c76b834bf00f54d988e239f
brgillian <brgillian@gmail.com>
2021-03-25 18:06:31 -0300

    Criada a tela de saldo e linkada com a Main

--- projeto/app/src/main/java/com/gillian/bancogg/Saldo.kt
+++ projeto/app/src/main/java/com/gillian/bancogg/Saldo.kt
@@ -1,11 +1,31 @@
 package com.gillian.bancogg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gillian.bancogg.databinding.ActivitySaldoBinding
 
 class Saldo : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySaldoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_saldo)
+        binding = ActivitySaldoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        val toolbar = binding.toolbarSaldo // guarda o id dentro de uma variável
+        toolbar.title = "Saldo"
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        toolbar.setBackgroundColor(getColor(R.color.light_blue))
+        //toolbar.setTitleMargin(400,0,400,0)
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_voltar))
+        toolbar.setNavigationOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
