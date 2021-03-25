91ac0727fca6de1d7850ffb6b8d9140a72a6ebad
brgillian <brgillian@gmail.com>
2021-03-25 20:49:35 -0300

    Criacao da tela transferencia e ligacao com a tela principal

--- projeto/app/src/main/java/com/gillian/bancogg/Transferencia.kt
+++ projeto/app/src/main/java/com/gillian/bancogg/Transferencia.kt
@@ -1,11 +1,31 @@
 package com.gillian.bancogg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gillian.bancogg.databinding.ActivityTransferenciaBinding
 
 class Transferencia : AppCompatActivity() {
+
+    private lateinit var binding:ActivityTransferenciaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_transferencia)
+        binding = ActivityTransferenciaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        val toolbar = binding.toolbarTransferencia
+        toolbar.title = "Transferências"
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        toolbar.setBackgroundColor(getColor(R.color.light_blue))
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_voltar))
+        toolbar.setNavigationOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
