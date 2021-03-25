bb3c8c8b2be0d3cad3252531c83baf63cb68bb3c
brgillian <brgillian@gmail.com>
2021-03-25 20:47:27 -0300

    Criacao do layout faturas e ligacao com a tela principal

---
+++ projeto/app/src/main/java/com/gillian/bancogg/Faturas.kt
@@ -0,0 +1,32 @@
+package com.gillian.bancogg
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gillian.bancogg.databinding.ActivityFaturasBinding
+
+class Faturas : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFaturasBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFaturasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide() // esconde a barra de ação
+
+        val toolbar = binding.toolbarFaturas
+        toolbar.title = "Faturas"
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        //toolbar.setTitleMargin(400, 0, 400, 0)
+        toolbar.setBackgroundColor(getColor(R.color.light_blue))
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_voltar))
+        toolbar.setNavigationOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
