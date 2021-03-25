086657390c5b8a816fe5c181365947ba4fd8362a
brgillian <brgillian@gmail.com>
2021-03-25 20:48:36 -0300

    Criacao da tela poupanca e ligacao com a tela principal

--- projeto/app/src/main/java/com/gillian/bancogg/Poupanca.kt
+++ projeto/app/src/main/java/com/gillian/bancogg/Poupanca.kt
@@ -1,11 +1,31 @@
 package com.gillian.bancogg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gillian.bancogg.databinding.ActivityPoupancaBinding
 
 class Poupanca : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPoupancaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_poupanca)
+        binding = ActivityPoupancaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        val toolbar = binding.toolbarPopanca
+        toolbar.title = "Poupança"
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        toolbar.setBackgroundColor(getColor(R.color.light_blue))
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_voltar))
+        toolbar.setNavigationOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
