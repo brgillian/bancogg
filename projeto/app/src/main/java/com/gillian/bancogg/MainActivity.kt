b90f2b84cd0f69271c77237058910d1dabe83e36
brgillian <brgillian@gmail.com>
2021-03-25 11:30:24 -0300

    Navegacao entre a tela principal e a de saldo estabelecida

--- projeto/app/src/main/java/com/gillian/bancogg/MainActivity.kt
+++ projeto/app/src/main/java/com/gillian/bancogg/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.gillian.bancogg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gillian.bancogg.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saldo.setOnClickListener{
+            val intent = Intent(this, Saldo::class.java)
+            startActivity(intent)
+        }
     }
 }
